65cfbd52931f9fdcbef1162c30937d28e8433070
Toan Ho <toanho1104@gmail.com>
2022-05-17 11:02:24 +0700

    add and config prettier sort

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,23 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  importOrder: [
+    '^react$',
+    '^react-native$',
+    '^(?!@graphql$|@graphql/(.*)$' +
+      '|@contexts$|./contexts$' +
+      '|@screens$' +
+      '|@components$|@common$' +
+      '|@utils$|@types$|@styles$|@navigation$|@services$|@constants$|@hooks$|@env$' +
+      '|./(.*)$|../(.*)$' +
+      '|@assets/(.*)$|@assets$)',
+    '^(@graphql$|@graphql/(.*)$)',
+    '^(@contexts$|./contexts$)',
+    '^@screens$',
+    '^(@common$|@components$|./components$)',
+    '^(@utils$|@types$|@styles$|@navigation$|@services$|@constants$|@hooks$|@env$)',
+    '^(./(.*)$|../(.*)$)',
+    '^(@assets/(.*)$|@assets$)',
+  ],
+  importOrderSeparation: true,
 };
