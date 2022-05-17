76eb1b57f7f5b9feea437ef8a49e498aa0fc654c
Toan Ho <toanho1104@gmail.com>
2022-05-17 10:58:12 +0700

    add and config module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@styles': './src/styles',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@contexts': './src/contexts',
+          '@utils': './src/utils',
+          '@navigation': './src/navigation',
+          '@types': './src/types',
+          '@constants': './src/constants',
+        },
+      },
+    ],
+  ],
 };
